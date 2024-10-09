a6869201dc172ea38b1812999219ff363115c696
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-10-09 17:23:48 +0100

    implement tip list deletions

--- test/tip-list.js
+++ test/tip-list.js
@@ -118,3 +118,142 @@ test('tip list - overwrite merge with offset', async function (t) {
   t.alike(tip.get(3), b4a.from('goodbye'))
   t.alike(tip.get(4), b4a.from('test'))
 })
+
+test('tip list - deletion', async function (t) {
+  const tip = new TipList()
+
+  tip.put(0, b4a.from('hello'))
+  tip.put(1, b4a.from('world'))
+  tip.put(2, b4a.from('goodbye'))
+
+  tip.delete(1, 10)
+
+  t.exception(() => tip.put(2, b4a.from('fail')), 'no put after deletion')
+
+  t.alike(tip.get(0), b4a.from('hello'))
+  t.alike(tip.get(1), null)
+  t.alike(tip.get(2), null)
+})
+
+test('tip list - deletion merge', async function (t) {
+  const tip = new TipList()
+
+  tip.put(0, b4a.from('hello'))
+  tip.put(1, b4a.from('world'))
+  tip.put(2, b4a.from('goodbye'))
+
+  {
+    const w = new TipList()
+    w.delete(1, 10)
+
+    t.exception(() => w.put(2, b4a.from('fail')), 'no put after deletion')
+
+    tip.merge(w)
+  }
+
+  t.alike(tip.get(0), b4a.from('hello'))
+  t.alike(tip.get(1), null)
+  t.alike(tip.get(2), null)
+})
+
+test('tip list - deletion on put batch', async function (t) {
+  const tip = new TipList()
+
+  {
+    const w = new TipList()
+    w.put(0, b4a.from('hello'))
+    w.put(1, b4a.from('world'))
+    w.put(2, b4a.from('goodbye'))
+    w.delete(2, 10)
+    tip.merge(w)
+  }
+
+  t.alike(tip.get(0), b4a.from('hello'))
+  t.alike(tip.get(1), b4a.from('world'))
+  t.alike(tip.get(2), null)
+})
+
+test('tip list - deletion with offset', async function (t) {
+  const tip = new TipList()
+  tip.put(4, b4a.from('hello'))
+  tip.put(5, b4a.from('world'))
+  tip.put(6, b4a.from('goodbye'))
+
+  {
+    const w = new TipList()
+    w.delete(5, 10)
+    tip.merge(w)
+  }
+
+  t.alike(tip.get(4), b4a.from('hello'))
+  t.alike(tip.get(5), null)
+  t.alike(tip.get(6), null)
+})
+
+test('tip list - overlap deletions in batch', async function (t) {
+  const tip = new TipList()
+  tip.put(0, b4a.from('hello'))
+  tip.put(1, b4a.from('world'))
+  tip.put(2, b4a.from('goodbye'))
+  tip.put(3, b4a.from('test'))
+
+  {
+    const w = new TipList()
+    w.delete(3, 4)
+    w.delete(2, 3)
+    w.delete(1, 2)
+    tip.merge(w)
+  }
+
+  t.alike(tip.get(0), b4a.from('hello'))
+  t.alike(tip.get(1), null)
+  t.alike(tip.get(2), null)
+  t.alike(tip.get(3), null)
+})
+
+test('tip list - overlap deletions in batch with offset', async function (t) {
+  const tip = new TipList()
+  tip.put(5, b4a.from('hello'))
+  tip.put(6, b4a.from('world'))
+  tip.put(7, b4a.from('goodbye'))
+  tip.put(8, b4a.from('test'))
+
+  {
+    const w = new TipList()
+    w.delete(8, 10)
+    w.delete(7, 8)
+    w.delete(6, 7)
+    tip.merge(w)
+  }
+
+  t.alike(tip.get(5), b4a.from('hello'))
+  t.alike(tip.get(6), null)
+  t.alike(tip.get(7), null)
+  t.alike(tip.get(8), null)
+})
+
+test('tip list - invalid deletion', async function (t) {
+  const tip = new TipList()
+  tip.put(4, b4a.from('hello'))
+  tip.put(5, b4a.from('world'))
+  tip.put(6, b4a.from('goodbye'))
+
+  await t.exception(() => tip.delete(0, 2))
+  await t.exception(() => tip.delete(8, 10))
+
+  {
+    const w = new TipList()
+    w.delete(0, 2)
+    t.exception(() => tip.merge(w))
+  }
+
+  {
+    const w = new TipList()
+    w.delete(8, 10)
+    t.exception(() => tip.merge(w))
+  }
+
+  t.alike(tip.get(4), b4a.from('hello'))
+  t.alike(tip.get(5), b4a.from('world'))
+  t.alike(tip.get(6), b4a.from('goodbye'))
+})

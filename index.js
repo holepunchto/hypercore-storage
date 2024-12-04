5554d78ca3e596d004cf563ebbf6a1d525a1afc5
Christophe Diederichs <45171645+chm-diederichs@users.noreply.github.com>
2024-12-04 14:36:31 +0000

    Revert "can explicitly pass snapshot shared length (#41)"
    This reverts commit 56f26612b1d289aca6f6b36640dc80cc02632ddd.

--- index.js
+++ index.js
@@ -487,16 +487,11 @@ class HypercoreStorage {
     return new MemoryOverlay(this)
   }
 
-  snapshot (sharedLength = -1) {
+  snapshot () {
     assert(this.destroyed === false)
     const s = new HypercoreStorage(this.root, this.discoveryKey, this.corePointer, this.dataPointer, this.db.snapshot())
 
-    for (const dep of this.dependencies) {
-      const length = sharedLength !== -1 && sharedLength < dep.length ? sharedLength : dep.length
-      s.dependencies.push({ data: dep.data, length })
-
-      if (length === sharedLength) break
-    }
+    for (const dep of this.dependencies) s.dependencies.push(dep)
 
     return s
   }

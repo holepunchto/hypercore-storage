276450de3fd9b44e97def4c3e923f10e1a17e0d9
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-01-15 10:45:40 +0100

    Add failing 'cannot write to snapshot' test

--- test/snapshot.js
+++ test/snapshot.js
@@ -25,3 +25,16 @@ test('read and write hypercore blocks from snapshot', async (t) => {
     t.is(b4a.toString(res[2]), 'block2', 'sanity check: does exist in non-snapshot core')
   }
 })
+
+test.solo('cannot write to snapshot', async (t) => {
+  const core = await createCore(t)
+
+  const snap = core.snapshot()
+  await writeBlocks(snap, 2)
+
+  // TODO: verify that writing to a snapshot shouldn't just throw
+  t.alike(await readBlocks(snap, 3), [null, null, null], 'Cannot write to a snaphot (noop)')
+
+  // TODO: clarify ([ <Buffer 62 6c 6f 63 6b 30>, <Buffer 62 6c 6f 63 6b 31>, null ])
+  t.alike(await readBlocks(core, 3), [null, null, null], 'Writing to a snapshot has no impact on actual core')
+})
